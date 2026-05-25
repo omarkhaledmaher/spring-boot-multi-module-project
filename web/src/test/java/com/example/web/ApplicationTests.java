07cc83c6601b89c33d8cd6338d8a6d0e211c8675
omar <omarkhaledmaher@gmail.com>
2026-05-25 13:28:06 +0300

    Removed application module. Moved Application.java to Web module

--- web/src/test/java/com/example/web/ApplicationTests.java
+++ web/src/test/java/com/example/web/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.application;
+package com.example.web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	void contextLoads() {}
 
 }
